c023b3e6b7c8cfdadc7a446e2c2bedf2f48bbac8
Vinod Durairaj <vinoddu@amazon.com>
2025-02-16 21:24:00 +0000

    resolve symllinks better

--- fzf-tab-source.plugin.zsh
+++ fzf-tab-source.plugin.zsh
@@ -1,34 +1,40 @@
-# https://github.com/zdharma-continuum/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
+0=":A"  # Resolve to absolute physical path
 
 function () {
-  local dir=${1:h} config_directory
-  local sources=($dir/sources/*.zsh)
-  # use user's sources to override this plugin's sources
-  zstyle -s ':fzf-tab:sources' config-directory config_directory &&
-    sources+=($config_directory/**/*.zsh(.N))
+  # Resolve dir to physical path immediately
+  local dir=${${1:h}:A} config_directory
+  
+  # Use N glob qualifier to prevent errors when no matches are found
+  local sources=(${dir}/sources/*.zsh(N:A))  # Added (N) qualifier
+  
+  # Resolve config directory symlinks too
+  zstyle -s ':fzf-tab:sources' config-directory config_directory
+  if [[ -n $config_directory ]]; then
+    config_directory=${config_directory:A}
+    sources+=(${config_directory}/**/*.zsh(.N:A))  # .N was already here
+  fi
 
-  # https://github.com/Freed-Wu/fzf-tab-source/issues/11
   # enable $group
   zstyle ':completion:*:descriptions' format %d
 
   local src line arr ctx flags
   for src in $sources; do
+    [[ -f $src ]] || continue  # Skip if file doesn't exist
+    
     while read -r line; do
-      # strip code to get magic comment
       arr=(${(@s. .)line##\# })
       ctx=${arr[1]}
       if [[ $ctx == ':fzf-tab:'* ]]; then
         break
       fi
     done < $src
-    zstyle $ctx fzf-preview "src="\""$src"\"" . "\""$dir"\""/functions/main.zsh"
+    
+    # Store resolved physical paths in zstyle
+    zstyle $ctx fzf-preview "src="\""${src:A}"\"" . "\""${dir:A}"\""/functions/main.zsh"
     flags=${arr[2]}
-    # https://github.com/Aloxaf/fzf-tab/issues/282
-    # https://github.com/junegunn/fzf/issues/2822
-    # $word = {2} make a wrong --preview-window=+{2}
-    # zstyle $ctx fzf-flags ${flags:---preview-window=+0}
+    
     if [[ -n $flags ]]; then
       zstyle $ctx fzf-flags $flags
     fi
